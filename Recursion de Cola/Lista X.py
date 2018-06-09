a52a9ac29f3932ca1d53a68bd9ed8af0410cba55
yenus210 <36671937+yenus210@users.noreply.github.com>
2018-06-09 15:47:00 -0600

    Delete Lista X.py

--- Recursion de Cola/Lista X.py
+++
@@ -1,9 +0,0 @@
-def ListaX (L):
-    if isinstance (L,list) and L!=[]:
-        return ListaX_aux (L,0,len (L),1)
-    else:return "Error"
-def ListaX_aux (L, ind, La, res):
-    if ind==La:
-        return res
-    else:
-        return ListaX_aux (L, ind+1, La, res * L[ind])
